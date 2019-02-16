1e83d63cc9c0d655969bf1949b1c604a9c501d59
Amycolle <amycolle348@gmail.com>
2019-02-16 10:14:51 +0000

    j'ai ajouter contact.php

---
+++ contact.php
@@ -0,0 +1,40 @@
+
+<form method ="post" action="traitement.php" >
+<h1>INFORMATIONS CLIENT</h1>
+
+
+
+   <p>
+	  <table>
+	  <tr> 
+	   <th> <label for="nom">nom:</label> </th>
+	   <th><input name =" nom" type="text" placeholder="nom...." id="nom" required /> </th>
+	  </tr>
+     <tr>
+	 <th> <label for="prenom">Prenom:</label></th>
+	 <th> <input name =" prenom" type="text" placeholder="prenom...." id="prenom" required />  </th>
+     </tr>
+    <tr>
+    	<th><label for="adresse">Adresse:</label></th>
+    	<th><input name =" adresse" type="text" placeholder="adresse" id="adresse" required /> </th>
+    </tr>
+
+    <tr>
+    	<th><label for="date de naissance">date denaissance:</label></th>
+    	<th><input name =" date de naissance" type="text" placeholder="date de naissance...." id="date de naissance" required /> </th>
+    </tr>
+	  <tr>
+	  	<th><label for="identifiant">identifiant:</label></th>
+	  	<th><input name ="identifiant" type="text" placeholder="identifiant...." id="identifiant" required /></th>
+
+	  </tr>
+
+	  </table>
+	 </p>
+	 </form>
+
+
+
+	  
+
+
